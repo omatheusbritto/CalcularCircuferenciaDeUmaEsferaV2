f72e4289e122bd631f4ee485d8628420604a0f6f
MATHEUS BRITTO <202407139019@alunos.estacio.br>
2024-07-29 09:39:40 -0300

    Adicionar arquivos de projeto.

---
+++ CalcularCircuferenciaDeUmaEsferaV2/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalcularCircuferenciaDeUmaEsferaV2
+{
+    class Calculadora
+    {
+        public double Pi = 3.1415;
+        public double r;
+        public double Circunferencia()
+        {
+            return 2.0 * Pi * r;
+        }
+        public double Volume()
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
---
+++ CalcularCircuferenciaDeUmaEsferaV2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Fazer um programa para ler um valor numérico qualquer, e daí mostrar quanto seria o valor
+// de uma circunferência e do volume de uma esferapara um raio daquele valor. Informar também
+// o valor de PI com duas casas decimais.
+
+using CalcularCircuferenciaDeUmaEsferaV2;
+using System;
+using System.Globalization;
+
+namespace CalcularCircuferencia
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Calculadora n1 = new Calculadora();
+
+            Console.Write("Entre com o valor: ");
+            n1.r = double.Parse(Console.ReadLine());
+            Console.WriteLine(" ");
+            double circ = n1.Circunferencia();
+            double vol = n1.Volume();
+            Console.WriteLine("A circuferencia de {0} é {1:F2}", n1.r, circ);
+            Console.WriteLine("A Volume de {0} é {1:F2}", n1.r, vol);
+            Console.WriteLine("Valor de Pi é de {0:F2}", n1.Pi);
+
+
+        }
+
+    }
+}
